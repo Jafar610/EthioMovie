3e543a4e7f424988371a41ff9f33c76f56bfd555
Jafar610 <jafarmohamed955@gmail.com>
2026-08-22 11:42:28 +0300

    login finished

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -9,19 +9,19 @@ import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 function Login() {
   return (
     <>
-      <div className="">
+      <div className="bg-[#010205] text-white w-full h-screen">
         <div>
           <div className="flex justify-between align-center py-4 px-20">
             <div className="flex justify-start align-center gap-2">
               <img src={logo} alt="" className="w-10 rounded-full" />
-              <h1 className="text-4xl font-bold text-black">
+              <h1 className="text-4xl font-bold text-white">
                 Ethio<span className="text-[#fab612]">Movie</span>
               </h1>
             </div>
             <div className="flex justify-center align-center gap-2 border border-[#fab612] rounded-lg px-4 py-2">
               <LanguageIcon />
               <select name="" id="" className="outline-none">
-                <option value="Eng" className="outline-none">
+                <option value="Eng" className="outline-none text-white">
                   English
                 </option>
               </select>
@@ -30,27 +30,27 @@ function Login() {
         </div>
 
         <div className="flex justify-between align-center px-20">
-          <div>
+          <div className="mt-50">
             <div>
-              <h2>Ethiopian Stories.</h2>
-              <h2>Endless Emotions.</h2>
-              <p>Discover and Watch Ethiopian Movies any time, anywhere.</p>
+              <h2 className="text-4xl font-bold py-2">Ethiopian Stories.</h2>
+              <h2 className="text-4xl font-bold pb-2 text-[#fab612]">Endless Emotions.</h2>
+              <p className="text-xl">Discover and Watch Ethiopian Movies <br /> any time, anywhere.</p>
             </div>
-            <div>
-              <div>
-                <OndemandVideoIcon />
-                <p>Unlimited Movies.</p>
-                <p>Watch all you love.</p>
+            <div className="flex justify-start align-center gap-10 py-6 text-[#fab612]">
+              <div className="">
+                <OndemandVideoIcon fontSize="large" />
+                <p className="text-[#f2f3f1] pt-2">Unlimited Movies.</p>
+                <p className="text-[#6c6d6e] text-sm">Watch all you love.</p>
               </div>
               <div>
-                <SmartphoneIcon />
-                <p>Anywhere.</p>
-                <p>On any device.</p>
+                <SmartphoneIcon fontSize="large"/>
+                <p className="text-[#f2f3f1] pt-2">Anywhere.</p>
+                <p className="text-[#6c6d6e] text-sm">On any device.</p>
               </div>
               <div>
-                <GppGoodOutlinedIcon />
-                <p>Safe & Secure.</p>
-                <p>Your data is protected.</p>
+                <GppGoodOutlinedIcon fontSize="large"/>
+                <p className="text-[#f2f3f1] pt-2" >Safe & Secure.</p>
+                <p className="text-[#6c6d6e] text-sm">Your data is protected.</p>
               </div>
             </div>
           </div>
