23b54de6e714be389641d8549a2fd8ec5fa5eb7c
Jafar610 <jafarmohamed955@gmail.com>
2026-08-23 18:48:22 +0300

    Premium card added.

---
+++ frontend/src/Components/PremiumCard/PremiumCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function PremiumCard() {
+  return (
+    <>
+      <div>
+        <div>
+            <div>
+            <img src="" alt="" />
+            <h1>Ethio<span>Movie</span></h1>
+            <h2>Unlimited Ethiopian Movies,anytime,anywhere.</h2>
+            <p>Join EthioMovie Premuim and Enjoy HD quality, ad-free,streaming on all your device.</p>
+        </div>
+
+        <div>
+            <img src="" alt="" />
+            <h2>Go to premium today!</h2>
+            <button>Start your free trial!</button>
+            <p>cancel anytime</p>
+        </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default PremiumCard
