f8a19d9c0fc9a158ee192e18f62338c472b4b32b
Jafar610 <jafarmohamed955@gmail.com>
2026-08-25 01:57:05 +0300

    Movie section started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,16 @@ import Movies from "./Components/Movies/Movies"
 import Navbar from "./Components/Navbar/Navbar"
 
 import Login from "./Pages/Login/Login"
+import MoviePage from "./Pages/MoviePage/MoviePage"
 function App() {
 
   return (
     <>
     <Navbar/>
-    <Hero/>
+    <MoviePage/>
+    {/* <Hero/>
     <Movies/>
-    <Footer/>
+    <Footer/> */}
     {/* <Login/> */}
     </>
   )
---
+++ frontend/src/Pages/MoviePage/MoviePage.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import TuneIcon from '@mui/icons-material/Tune';
+import movies from '../../API/data';
+import StarIcon from '@mui/icons-material/Star';
+function MoviePage() {
+  return (
+    <>
+     <div className='px-20'>
+        <div>
+            <h1>Movies</h1>
+            <div className='flex justify-center align-center gap-10'>
+                <select name="" id="">
+                    <option value="">All Genres</option>
+                </select>
+
+                <select name="" id="">
+                    <option value="">All Years</option>
+                </select>
+
+                <select name="" id="">
+                    <option value="">Popular</option>
+                </select>
+
+                <div>
+                 <TuneIcon/>
+                 <button>Filters</button>
+                </div>
+            </div>
+
+            <div>
+                <h4>Showing 1-20 of 320 Movies.</h4>
+                <div className='grid grid-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 relative'>
+                    {
+                        movies.map((movie, index)=>(
+                            <div key={index} className='rounded-xl overflow-hidden hover:scale-104 relative '>
+                                <img src={movie.image} alt="" className='rounded-xl' />
+
+                                <div className='absolute insert-0 bg-gradient-to-t from-black/90 via-black/40 to-transparent'>hello</div>
+
+                                <div className='absolute bottom-0 w-full px-2 py-2'>
+                                    <h2 className='text-lg font-bold text-white'>{movie.title}</h2>
+                                <div className='flex justify-between align-center '>
+                                    <div><p>2022.Drama</p></div>
+                                    <div className='flex'>
+                                        <StarIcon/>
+                                        <p>4.5</p>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default MoviePage
