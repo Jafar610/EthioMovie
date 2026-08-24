d87419965412127c46e5cf9e611e6aea81d67c16
Jafar610 <jafarmohamed955@gmail.com>
2026-08-25 02:03:12 +0300

    MoviePage started

--- frontend/src/Pages/MoviePage/MoviePage.jsx
+++ frontend/src/Pages/MoviePage/MoviePage.jsx
@@ -29,19 +29,19 @@ function MoviePage() {
 
             <div>
                 <h4>Showing 1-20 of 320 Movies.</h4>
-                <div className='grid grid-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 relative'>
+                <div className='grid grid-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4'>
                     {
                         movies.map((movie, index)=>(
                             <div key={index} className='rounded-xl overflow-hidden hover:scale-104 relative '>
                                 <img src={movie.image} alt="" className='rounded-xl' />
 
-                                <div className='absolute insert-0 bg-gradient-to-t from-black/90 via-black/40 to-transparent'>hello</div>
+                                <div className='absolute inset-0 bg-gradient-to-t from-black/90 via-black/40 to-transparent'></div>
 
                                 <div className='absolute bottom-0 w-full px-2 py-2'>
                                     <h2 className='text-lg font-bold text-white'>{movie.title}</h2>
-                                <div className='flex justify-between align-center '>
+                                <div className='flex justify-between align-center text-gray-300'>
                                     <div><p>2022.Drama</p></div>
-                                    <div className='flex'>
+                                    <div className='flex text-yellow-500'>
                                         <StarIcon/>
                                         <p>4.5</p>
                                     </div>
